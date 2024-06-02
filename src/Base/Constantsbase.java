4649a34ad900878833c3993ae15e3ed3d3f634eb
HemaRengarajan <hemalect@gmail.com>
2024-06-02 14:21:38 +0100

    Dependencies is added

--- src/Base/Constantsbase.java
+++ src/Base/Constantsbase.java
@@ -5,8 +5,5 @@ public class Constantsbase
 
 	
 	public static String BASE_URL = "https://www.facebook.com/";
-	public static void main(String args[])
-	{
-		System.out.println("Baseurl = " +BASE_URL);
-	}
+	
 }
